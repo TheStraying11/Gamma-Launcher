6a215a41c162442ea2f3064da849c4d2d097602d
TheStraying11 <jtrycroft@gmail.com>
2019-05-01 17:14:53 +0100

    changed file structure, added testpack.json and username submition and saving - also switching computers (yes again)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class init_json: #division class for initialising modpack_list.json
 	modpack_menu = QListWidget()
 
 	for i in enumerate(modpack_list['modpacks']):
-		modpack_menu.addItem(i[1][0]['name'])
+		modpack_menu.addItem(i[1][0]["name"])
 
 class windows:
 
@@ -44,11 +44,11 @@ class windows:
 		system_definition_input = QComboBox()
 		system_definition_input.addItems(["Linux (Standalone)","Linux (Steam)", "MacOS (Standalone)","MacOS (Steam)", "Windows (Standalone)", "Windows (Steam)"])
 
-		submit_button_0 = QPushButton('Submit')
-		submit_button_0.clicked.connect(lambda: functions.submit_0())
+		submit_system_button = QPushButton('Submit')
+		submit_system_button.clicked.connect(lambda: functions.submit_system())
 
 		layout_0.addWidget(system_definition_input)
-		layout_0.addWidget(submit_button_0)
+		layout_0.addWidget(submit_system_button)
 
 		system_definition_window.setLayout(layout_0)
 		system_definition_window.setGeometry(x-125,y-56,250,112)
@@ -73,8 +73,8 @@ class windows:
 		browse_application = QPushButton('Browse')
 		browse_application.clicked.connect(lambda: functions.browse_1())
 
-		submit_button_1 = QPushButton('Submit')
-		submit_button_1.clicked.connect(lambda: functions.submit_1())
+		submit_folder_button = QPushButton('Submit')
+		submit_folder_button.clicked.connect(lambda: functions.submit_folder())
 
 		default_continue_label = QLabel("If you don't know what this means, the defaults are probably fine.")
 
@@ -91,17 +91,34 @@ class windows:
 		layout_1.addWidget(application_folder_label)
 		layout_1.addLayout(application_group)
 		layout_1.addWidget(default_continue_label)
-		layout_1.addWidget(submit_button_1)
+		layout_1.addWidget(submit_folder_button)
 
 		folder_definition_window.setLayout(layout_1)
 		folder_definition_window.setGeometry(x-247,y-116,494,232)
+
+	class username: #user defines username
+		username_definition_window = QWidget()
+
+		layout_2 = QVBoxLayout()
+
+		username_input = QLineEdit()
+		username_input.setPlaceholderText('Enter your factorio username')
+
+		submit_username_button = QPushButton('Submit')
+		submit_username_button.clicked.connect(lambda: functions.submit_username())
+		
+		layout_2.addWidget(username_input)
+		layout_2.addWidget(submit_username_button)
+
+		username_definition_window.setLayout(layout_2)
+		username_definition_window.setGeometry(x-125,y-56,250,112)
 
 	class main: #division class for main window with all modpacks listed
 		modpack_menu = init_json.modpack_menu
 
 		main_window = QWidget()
 
-		layout_2 = QHBoxLayout()
+		layout_3 = QHBoxLayout()
 
 		add_button = QPushButton('Add from file')
 		add_button.clicked.connect(lambda: functions.add_list())
@@ -114,10 +131,10 @@ class windows:
 		add_button_vertical = QVBoxLayout()
 		add_button_vertical.addWidget(add_button)
 
-		layout_2.addLayout(list_vertical)
-		layout_2.addLayout(add_button_vertical)
+		layout_3.addLayout(list_vertical)
+		layout_3.addLayout(add_button_vertical)
 
-		main_window.setLayout(layout_2)
+		main_window.setLayout(layout_3)
 		main_window.setGeometry(x-247,y-69,494,138)
 
 	class add: #division class for 'add modpack from file' window
@@ -129,27 +146,27 @@ class windows:
 		modpack_json_input = QLineEdit()
 		browse_json = QPushButton('Browse')
 		browse_json.clicked.connect(lambda: functions.browse_2())
-		submit_button_2 = QPushButton('Submit')
-		submit_button_2.clicked.connect(lambda: functions.submit_2())
+		submit_add_button = QPushButton('Submit')
+		submit_add_button.clicked.connect(lambda: functions.submit_add())
 
 		json_group = QHBoxLayout()
 		json_group.addWidget(modpack_json_input)
 		json_group.addWidget(browse_json)
 
-		layout_3 = QVBoxLayout()
-		layout_3.addWidget(modpack_name_label)
-		layout_3.addWidget(modpack_name_input)
-		layout_3.addWidget(modpack_json_label)
-		layout_3.addLayout(json_group)
-		layout_3.addWidget(submit_button_2)
+		layout_4 = QVBoxLayout()
+		layout_4.addWidget(modpack_name_label)
+		layout_4.addWidget(modpack_name_input)
+		layout_4.addWidget(modpack_json_label)
+		layout_4.addLayout(json_group)
+		layout_4.addWidget(submit_add_button)
 
-		add_window.setLayout(layout_3)
+		add_window.setLayout(layout_4)
 		add_window.setGeometry(x-275,y-104,550,208)
 
 	class launch: #division class for the launch window
 		launch_window = QWidget()
 
-		layout_4 = QVBoxLayout()
+		layout_5 = QVBoxLayout()
 
 class functions:
 	def browse_0(): #browse button for data folder text box
@@ -161,7 +178,7 @@ class functions:
 	def browse_2(): #browse button for json file text box
 		windows.add.modpack_json_input.setText(QFileDialog.getOpenFileName()[0])
 
-	def submit_0(): #submit button for system definition window
+	def submit_system(): #submit button for system definition window
 		wf = windows.folder
 
 		windows.system.system_definition_window.hide()
@@ -171,17 +188,25 @@ class functions:
 
 		wf.folder_definition_window.show()
 
-	def submit_1(): #submit button for folder definition window
+	def submit_folder(): #submit button for folder definition window
 		wf = windows.folder
 		wf.folder_definition_window.hide()
 		wf.application_folder_path = windows.folder.application_folder_input.text()
 		wf.data_folder_path = windows.folder.data_folder_input.text()
-		windows.main.main_window.show()
+
+		with open('settings.json') as settings_json:
+			settings = json.load(settings_json)
+		settings_json.close()
+
+		if (settings["username"] == ''):
+			windows.username.username_definition_window.show()
+		else:
+			windows.main.main_window.show()
 
 		print('data folder is '+wf.data_folder_path)
 		print('application folder is '+wf.application_folder_path)
 
-	def submit_2(): #submit button for 'add from file' window
+	def submit_add(): #submit button for 'add from file' window
 		wa = windows.add
 		modpack_list = init_json.modpack_list
 		json_file = init_json.json_file
@@ -214,6 +239,27 @@ class functions:
 		
 		for i in enumerate(modpack_list['modpacks']):
 			modpack_menu.addItem(i[1][0]["name"])
+
+	def submit_username():
+		windows.username.username_definition_window.hide()
+
+		usr = windows.username.username_input.text()
+
+		print(usr)
+
+		with open('settings.json') as settings_json:
+			settings = json.load(settings_json)
+		settings_json.close()
+		if (usr != ''):
+			settings["username"] = usr
+
+		with open('settings.json', 'w') as outfile:
+			json.dump(settings, outfile, indent=4)
+		outfile.close()
+
+
+		windows.main.main_window.show()
+
 
 	def add_list(): #'add from file' button
 		windows.add.add_window.show()
